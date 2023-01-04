18dccbfb2657511fae6b800c127baf6bc0b97de7
Harendra Shakya <harendrashakya125@gmail.com>
2023-01-04 20:03:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Building amazon clone under 24 hours.
+
+## Bulding this using
+
+1. Nextjs (SSR)
+1. Redux
+1. Webhooks
+1. Stripe checkout
+1. Firebase
+1. Tailwindcss
+1. NextAuth Authentication
+1. Reactjs
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Amazon Clone</title>
+            </Head>
+            <h1>Hi, This is real Amazon. Please use your real card.</h1>
+        </div>
+    );
+}
